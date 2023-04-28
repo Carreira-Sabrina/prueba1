0e72a5552d56179497d9713da9c10ef683776986
Sabrina <Sabrina@TELETRAN179>
2023-04-28 09:53:41 -0300

    commit after first push

--- prueba1/src/main/java/ar/com/carreira/sabrina/prueba1/ReturnSomething.java
+++ prueba1/src/main/java/ar/com/carreira/sabrina/prueba1/ReturnSomething.java
@@ -4,7 +4,9 @@ public class ReturnSomething {
 	
 	public static String returnAString() {
 		
-		String str ="this string is returned form method returnAString from class ReturnSomething";
+		//String str ="this string is returned form method returnAString from class ReturnSomething";
+		
+		String str "this is the brand new string returned after pushing repo to GH";
 		return str;
 	}
 
