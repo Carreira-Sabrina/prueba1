c03883d99638e126dc7ad78da83d1d9b73d2fabf
Sabrina <Sabrina@TELETRAN179>
2023-04-28 09:49:29 -0300

    a message from first commit

---
+++ prueba1/src/main/java/ar/com/carreira/sabrina/prueba1/App.java
@@ -0,0 +1,17 @@
+package ar.com.carreira.sabrina.prueba1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        String str = ReturnSomething.returnAString();
+        
+        System.out.println(str);
+    }
+}
---
+++ prueba1/src/main/java/ar/com/carreira/sabrina/prueba1/ReturnSomething.java
@@ -0,0 +1,11 @@
+package ar.com.carreira.sabrina.prueba1;
+
+public class ReturnSomething {
+	
+	public static String returnAString() {
+		
+		String str ="this string is returned form method returnAString from class ReturnSomething";
+		return str;
+	}
+
+}
